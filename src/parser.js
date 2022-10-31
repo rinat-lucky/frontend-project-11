4a3b35d0d910aa107f9da5a3b9f31419a6331c83
rinat-u <rinat1kam@mail.ru>
2022-10-31 08:39:00 +0500

    step-4 in progress

---
+++ src/parser.js
@@ -0,0 +1,32 @@
+import uniqueId from 'lodash/uniqueId.js';
+
+const parser = new DOMParser();
+
+export default (data) => {
+  const xmlDoc = parser.parseFromString(data, 'application/xml');
+
+  const feedTitle = xmlDoc.querySelector('title').textContent;
+  const feedDescr = xmlDoc.querySelector('description').textContent;
+  const id = uniqueId();
+
+  const posts = [];
+
+  const postsElems = xmlDoc.querySelectorAll('item');
+  postsElems.forEach((post) => {
+    const postTitle = post.querySelector('title').textContent;
+    const postDescr = post.querySelector('description').textContent;
+    const postLink = post.querySelector('link').textContent;
+    const postID = uniqueId();
+    const feedID = id;
+    posts.push({
+      postID, postTitle, postDescr, postLink, feedID,
+    });
+  });
+
+  return {
+    feed: {
+      id, feedTitle, feedDescr,
+    },
+    posts,
+  };
+};
